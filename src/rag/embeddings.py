3f70626b1760469fdcae0d77b8e93ee7876a10ce
paramjit <sparamjit92@gmail.com>
2026-05-25 11:35:01 +0530

    more files in rag

--- src/rag/embeddings.py
+++ src/rag/embeddings.py
@@ -7,6 +7,7 @@ client = voyageai.Client()
 
 
 def generate_embedding(chunks, model="voyage-4-large", input_type="query"):
+    is_list=isinstance(chunks, list)
     input= chunks if is_list else [chunks]
     result= client.embed(input, model=model, input_type=input_type)
     embeddings= result.embeddings if is_list else result.embeddings[0]
