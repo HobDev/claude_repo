3728286631194e5d4ee423cf9ea4fc0e461fabba
paramjit <sparamjit92@gmail.com>
2026-06-10 19:58:14 +0530

    improvement

--- src/mcp/mcp_server.py
+++ src/mcp/mcp_server.py
@@ -14,7 +14,7 @@ docs = {
     "spec.txt": "These specifications define the technical requirements for the equipment.",
 }
 
-# TODO: Write a resource to return the contents of a particular doc
+
 # TODO: Write a prompt to summarize a doc
 
 
@@ -50,12 +50,13 @@ def edit_document(
 
     docs[doc_id] = docs[doc_id].replace(old_str, new_str)
 
-
+# a resource to return all doc id's
 @mcp.resource("docs://documents", mime_type="application/json")
 def list_docs() -> list[str]:
     return list(docs.keys())
 
-# a resource to return all doc id's
+
+# a resource to return the contents of a particular doc
 @mcp.resource("docs://documents/{doc_id}", mime_type="text/plain")
 def fetch_doc(doc_id: str) -> str:
     if doc_id not in docs:
